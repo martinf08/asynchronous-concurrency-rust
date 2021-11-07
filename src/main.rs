1076749d310f287a4f76c46839f23162993cbfd0
Martin <martin.fontana08@gmail.com>
2021-11-07 17:50:44 +0100

    Multi thread scraping

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,134 @@
 use async_recursion::async_recursion;
-use reqwest::{Client, Request, RequestBuilder};
+use reqwest::Client;
 use select::document::Document;
-use select::node::Node;
 use select::predicate::*;
-use select::selection::Selection;
 use std::collections::HashMap;
 use std::error::Error;
-use std::time::Duration;
-use tokio::io::AsyncReadExt;
-use tokio::macros::support::thread_rng_n;
 use tokio::runtime;
 use tokio::sync::mpsc;
-use tokio::time::*;
 
 const BASE_URI: &str = "https://www.webscraper.io";
+
+#[derive(Debug)]
+struct ProductPage {
+    category: String,
+    sub_category: String,
+    url: String,
+}
+
+async fn get_categories(
+    client: &Client,
+) -> Result<HashMap<String, HashMap<String, String>>, Box<dyn Error>> {
+    let res = client
+        .get(format!("{}/test-sites/e-commerce/static", self::BASE_URI))
+        .send()
+        .await?
+        .text()
+        .await?;
+
+    let document = Document::from(res.as_str());
+    let maybe_menu = document.find(Attr("id", "side-menu")).last();
+    if maybe_menu.is_none() {
+        return Ok(HashMap::new());
+    }
+
+    let menu = maybe_menu.unwrap();
+
+    let urls = menu
+        .find(Name("a"))
+        .filter(|n| n.attr("href").is_some())
+        .map(|n| {
+            (
+                n.text().trim().to_string(),
+                n.attr("href").unwrap().to_string(),
+            )
+        })
+        .collect::<HashMap<String, String>>();
+
+    let mut result = HashMap::new();
+
+    for (category, uri) in urls {
+        let res = client
+            .get(format!("{}{}", self::BASE_URI, uri))
+            .send()
+            .await?
+            .text()
+            .await?;
+
+        let document = Document::from(res.as_str());
+        let maybe_menu = document.find(Attr("id", "side-menu")).last();
+        if maybe_menu.is_none() {
+            return Ok(HashMap::new());
+        }
+
+        let menu = maybe_menu.unwrap();
+        menu.find(And(Name("a"), Class("subcategory-link")))
+            .filter(|n| n.attr("href").is_some())
+            .map(|n| {
+                (
+                    n.text().trim().to_string(),
+                    n.attr("href").unwrap().to_string(),
+                )
+            })
+            .for_each(|(k, v)| {
+                result
+                    .entry(category.clone())
+                    .or_insert_with(HashMap::new)
+                    .insert(k, v);
+            });
+    }
+
+    return Ok(result);
+}
+
+#[async_recursion]
+async fn get_product_batches(
+    client: Client,
+    category: String,
+    sub_category: String,
+    uri: String,
+    i: i32,
+) -> Result<Vec<ProductPage>, reqwest::Error> {
+    let res = client
+        .get(format!("{}{}?page={}", self::BASE_URI, uri, i))
+        .send()
+        .await?
+        .text()
+        .await?;
+
+    let mut product_pages = Vec::new();
+    Document::from(res.as_str())
+        .find(
+            And(Name("div"), Class("row"))
+                .descendant(Class("thumbnail"))
+                .descendant(Name("a")),
+        )
+        .filter_map(|n| n.attr("href"))
+        .map(|a| a.to_string())
+        .for_each(|url| {
+            product_pages.push(ProductPage {
+                category: category.clone(),
+                sub_category: sub_category.clone(),
+                url,
+            })
+        });
+
+    Ok(product_pages)
+}
+
+async fn is_last_page(res: String, current: i32) -> bool {
+    if let Some(node) = Document::from(res.as_str())
+        .find(And(Name("ul"), Class("pagination")).descendant(Name("a")))
+        .filter(|n| n.text().parse::<i32>().is_ok())
+        .last()
+    {
+        if let Ok(page) = node.text().parse::<i32>() {
+            return current > page;
+        }
+    }
+
+    false
+}
 
 #[tokio::main]
 async fn main() {
@@ -28,74 +143,65 @@ async fn main() {
 
     let (tx, mut rx) = mpsc::channel(100);
 
-    for (category, uri) in categories {
-        let tx_clone = tx.clone();
-        let client_clone = client.clone();
+    for (category, uris) in categories {
+        for (sub_category, uri) in uris {
+            'inner: for i in 0.. {
+                let tx_clone = tx.clone();
+                let client_clone = client.clone();
+                let category_clone = category.clone();
+                let sub_category_clone = sub_category.clone();
+                let uri_clone = uri.clone();
 
-        cpu_pool.spawn(async move {
-            dbg!(&category);
-            if let Err(_) = tx_clone
-                .send(get_product_batches(client_clone, category, uri, 1).await)
-                .await
-            {
-                println!("Receiver dropped");
-                return;
+                let finish = cpu_pool.spawn(async move {
+                    let res = client_clone
+                        .get(format!("{}{}", self::BASE_URI, uri_clone.clone()))
+                        .send()
+                        .await
+                        .unwrap()
+                        .text()
+                        .await
+                        .unwrap();
+
+                    if is_last_page(res, i).await {
+                        return false;
+                    }
+
+                    if let Err(_) = tx_clone
+                        .send(
+                            get_product_batches(
+                                client_clone,
+                                category_clone,
+                                sub_category_clone,
+                                uri_clone,
+                                i,
+                            )
+                            .await,
+                        )
+                        .await
+                    {
+                        println!("Receiver dropped");
+                        return false;
+                    }
+
+                    drop(tx_clone);
+                    return true;
+                });
+
+                if !finish.await.unwrap() {
+                    break 'inner;
+                }
             }
-        });
-    }
-    while let Some(products_urls) = rx.recv().await {
-        if let Ok(urls) = products_urls {
-            dbg!(urls);
         }
     }
-}
 
-async fn get_categories(client: &Client) -> Result<HashMap<String, String>, Box<dyn Error>> {
-    let res = client
-        .get(format!("{}/test-sites/e-commerce/static", self::BASE_URI))
-        .send()
-        .await?
-        .text()
-        .await?;
+    drop(tx);
+    while let Some(products_urls) = rx.recv().await {
+        if let Ok(product_page_batch) = products_urls {
+            for product_page in product_page_batch {
+                cpu_pool.spawn(async move { dbg!(product_page) });
+            }
+        }
+    }
 
-    Ok(Document::from(res.as_str())
-        .find(Attr("id", "side-menu").descendant(Name("a")))
-        .into_selection()
-        .iter()
-        .filter(|n| n.attr("href").is_some())
-        .map(|n| {
-            (
-                n.text().trim().to_string(),
-                n.attr("href").unwrap().to_string(),
-            )
-        })
-        .collect::<HashMap<String, String>>())
-}
-
-#[async_recursion]
-async fn get_product_batches(
-    client: Client,
-    category_name: String,
-    uri: String,
-    page: u32,
-) -> Result<Vec<String>, reqwest::Error> {
-    dbg!(&uri);
-    let res = client
-        .get(format!("{}{}", self::BASE_URI, uri))
-        .send()
-        .await?
-        .text()
-        .await?;
-
-    let product_urls = Document::from(res.as_str())
-        .find(
-            And(Name("div"), Class("row"))
-                .descendant(Class("thumbnail"))
-                .descendant(Name("a")),
-        )
-        .filter_map(|n| n.attr("href"))
-        .map(|a| a.to_string())
-        .collect::<Vec<String>>();
-
-    Ok(product_urls)
+    cpu_pool.shutdown_background();
 }
